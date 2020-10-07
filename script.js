6e6fcd543cf0c64ab45520f50a3e777250012712
Shannon Quinn <shannon.quinn91@gmail.com>
2020-10-07 12:53:27 -0400

    Added if statements checking on prompts and confirms

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function writePassword() {
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q,', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var specCharacters = ['!', '@', '#', '$', '?', '~']; 
+var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
 function generatePassword() {
@@ -21,14 +22,26 @@ function generatePassword() {
       alert('Your password length must be a number between 8 and 129')
       return; 
     }
-  var includeLowerCase = confirm('Do you want lower case letters in your password?');
-  var includeUpperCase = confirm('Do you want upper case letters in your password?');
-  var includeSpecCharacters = confirm('Do you want to include special characters in your password?');
-  var includeNums = confirm('Do you want to include numbers in your password?');
-  var text = Math.floor(Math.random() * lowerCase.length);
-  var first = lowerCase[text];
-  return first;
+    if (length < 8){
+      alert('Your password must be at least 8 characters.')
+      return;
+    }
+    if (length < 128){
+      alert('Your password must be less than 128 characters.')
+      return;
+    }
+  var includeLowerCase = confirm('Click OK to include lower case characters in your password.');
+  var includeUpperCase = confirm('Click OK to include upper case characters in your password.');
+  var includeSpecCharacters = confirm('Click OK to include special characters in your password.');
+  var includeNums = confirm('Click OK to include numeric characters in your password.');
   
+  if (includeLowerCase === false && 
+    includeUpperCase === false && 
+    includeSpecCharacters === false && 
+    includeNums === false) {
+    alert('Please click OK on at least one character type to generate password.')
+    return;
+    }
 }
 
 
