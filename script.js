9100f84994122781cda0c3933530cda4b67a518e
Shannon Quinn <shannon.quinn91@gmail.com>
2020-10-07 12:21:58 -0400

    Added all necessary prompts and confirm alerts

--- script.js
+++ script.js
@@ -10,12 +10,25 @@ function writePassword() {
 
 }
 
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q,', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var specCharacters = ['!', '@', '#', '$', '?', '~']; 
+
 
 function generatePassword() {
-  var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  var text = Math.floor(Math.random() * lowerCase.length)
+  var length = prompt('How many characters do you want in your password?');
+    if (isNaN(length)){
+      alert('Your password length must be a number between 8 and 129')
+      return; 
+    }
+  var includeLowerCase = confirm('Do you want lower case letters in your password?');
+  var includeUpperCase = confirm('Do you want upper case letters in your password?');
+  var includeSpecCharacters = confirm('Do you want to include special characters in your password?');
+  var includeNums = confirm('Do you want to include numbers in your password?');
+  var text = Math.floor(Math.random() * lowerCase.length);
   var first = lowerCase[text];
   return first;
+  
 }
 
 
