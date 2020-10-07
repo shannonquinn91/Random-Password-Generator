a29ff0a7e4fd76c58d132652d7a8c69422cac598
Shannon Quinn <shannon.quinn91@gmail.com>
2020-10-07 11:40:51 -0400

    Got a random letter from an array

--- script.js
+++ script.js
@@ -10,5 +10,14 @@ function writePassword() {
 
 }
 
+
+function generatePassword() {
+  var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var text = Math.floor(Math.random() * lowerCase.length)
+  var first = lowerCase[text];
+  return first;
+}
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
